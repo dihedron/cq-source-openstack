4b0f3da83fee225d3186f53327867ea966c23ef2
Francesco Fuggitti <francesco.fuggitti@bancaditalia.it>
2024-02-02 10:20:25 +0100

    add blockstorage/qos

---
+++ resources/services/blockstorage/qos.go
@@ -0,0 +1,54 @@
+package blockstorage
+
+import (
+	"context"
+
+	"github.com/cloudquery/plugin-sdk/v4/schema"
+	"github.com/cloudquery/plugin-sdk/v4/transformers"
+	"github.com/dihedron/cq-plugin-utils/format"
+	"github.com/dihedron/cq-source-openstack/client"
+	"github.com/gophercloud/gophercloud/openstack/blockstorage/v3/qos"
+)
+
+func QoSs() *schema.Table {
+	return &schema.Table{
+		Name:     "openstack_qos",
+		Resolver: fetchQoSs,
+		Transform: transformers.TransformWithStruct(
+			&qos.QoS{},
+			transformers.WithPrimaryKeys("ID"),
+		),
+	}
+}
+
+func fetchQoSs(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan<- interface{}) error {
+	api := meta.(*client.Client)
+
+	cinder, err := api.GetServiceClient(client.BlockStorageV3)
+	if err != nil {
+		api.Logger().Error().Err(err).Msg("error retrieving client")
+		return err
+	}
+
+	opts := qos.ListOpts{}
+
+	allPages, err := qos.List(cinder, opts).AllPages()
+	if err != nil {
+		api.Logger().Error().Err(err).Str("options", format.ToPrettyJSON(opts)).Msg("error listing qos with options")
+		return err
+	}
+	allQoS, err := qos.ExtractQoS(allPages)
+	if err != nil {
+		panic(err)
+	}
+	for _, qos := range allQoS {
+		if ctx.Err() != nil {
+			api.Logger().Debug().Msg("context done, exit")
+			break
+		}
+		qos := qos
+		api.Logger().Debug().Str("data", qos.ID).Msg("streaming qos")
+		res <- qos
+	}
+	return nil
+}
